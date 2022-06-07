38f268990e5c236c3f212bb7368462b51dedfc26
Matt Mackay <mattem@gmail.com>
2022-06-07 12:48:30 -0400

    feat: add support for variable and location expansion on `env` attr (#47)

---
+++ py/tests/py-test/test_env_vars.py
@@ -0,0 +1,17 @@
+import os
+
+
+def test_env(env, expected):
+    assert env in os.environ, f"Expected environ to have key '{env}'"
+
+    _actual = os.environ.get(env)
+    assert _actual == expected, f"Expected environ key '{env}' to equal '{expected}', but got '{_actual}'"
+
+
+test_env('ONE', 'un')
+test_env('TWO', 'deux')
+test_env('LOCATION', "py/tests/py-test/test_env_vars.py")
+test_env('DEFINE', "SOME_VALUE")
+test_env('BAZEL_TARGET', "//py/tests/py-test:test_env_vars")
+test_env('BAZEL_WORKSPACE', "aspect_rules_py")
+test_env('BAZEL_TARGET_NAME', "test_env_vars")
