7a75c26c056aba16028230d442e3e2d5b5b3bcd7
Alex Eagle <alex@aspect.dev>
2023-11-01 07:48:33 -0700

    refactor: put 'git archive' config in .gitattributes
    Makes it match bazelbuild/rules_proto#189

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,15 +2,14 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Don't include examples in the distribution artifact, just to reduce size
-echo >.git/info/attributes "examples export-ignore"
-
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_py-${TAG:1}"
 ARCHIVE="rules_py-$TAG.tar.gz"
+
+# NB: configuration for 'git archive' is in /.gitattributes
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
