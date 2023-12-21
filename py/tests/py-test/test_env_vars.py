1a7f88e64c0250f172bb24a318a3988804da64cc
Alex Eagle <alex@aspect.dev>
2023-12-21 14:35:00 -0800

    chore: minimal changes required to upgrade to Bazel 7
    In follow-up changes:
    - enable bzlmod
    - empty out the WORKSPACE file

--- py/tests/py-test/test_env_vars.py
+++ py/tests/py-test/test_env_vars.py
@@ -13,5 +13,12 @@ test_env('TWO', 'deux')
 test_env('LOCATION', "py/tests/py-test/test_env_vars.py")
 test_env('DEFINE', "SOME_VALUE")
 test_env('BAZEL_TARGET', "//py/tests/py-test:test_env_vars")
-test_env('BAZEL_WORKSPACE', "aspect_rules_py")
+# With bzlmod enabled, the main workspace has this hard-coded name.
+# See https://bazelbuild.slack.com/archives/C014RARENH0/p1702318129963129?thread_ts=1702288944.560349&cid=C014RARENH0
+# Xudong Yang:
+# the 'workspace name' is an unfortunate concept laden with historical baggage.
+# The better question here might be -- what do you intend to do with the 'workspace name'?
+# the name of the execution root directory will always be _main when Bzlmod is enabled.
+# similarly, the runfiles dir prefix for the main repo will always be _main.
+test_env('BAZEL_WORKSPACE', "_main")
 test_env('BAZEL_TARGET_NAME', "test_env_vars")
