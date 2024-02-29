3c3b9e04b49d6b100ae00f2ad0f590e5b86a41d0
Alex Eagle <alex@aspect.dev>
2024-02-29 08:56:30 -0800

    feat: GHA release automation for rust bins

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,15 +2,22 @@
 
 set -o errexit -o nounset -o pipefail
 
+# Release binaries were downloaded in the GHA workflow.
+shasum -a 256 artifacts/*
+
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_py-${TAG:1}"
 ARCHIVE="rules_py-$TAG.tar.gz"
+ARCHIVE_TMP=$(mktemp)
 
 # NB: configuration for 'git archive' is in /.gitattributes
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} > $ARCHIVE_TMP
+tar --delete ${PREFIX}/tools/integrity.bzl $ARCHIVE_TMP
+# tar --append 
+gzip < $ARCHIVE_TMP > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
