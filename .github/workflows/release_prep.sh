e6dfa9eb77e008a8d402bc56692becb31a509fae
Alex Eagle <alex@aspect.dev>
2024-02-29 08:56:30 -0800

    feat: GHA release automation for rust bins

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -8,9 +8,42 @@ TAG=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_py-${TAG:1}"
 ARCHIVE="rules_py-$TAG.tar.gz"
+ARCHIVE_TMP=$(mktemp)
 
 # NB: configuration for 'git archive' is in /.gitattributes
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} > $ARCHIVE_TMP
+
+############
+# Patch up the archive to have integrity hashes for built binaries that we downloaded in the GHA workflow.
+# Now that we've run `git archive` we are free to pollute the working directory.
+
+# Delete the placeholder file
+tar --file $ARCHIVE_TMP --delete ${PREFIX}/tools/integrity.bzl
+
+# Add trailing newlines to sha256 files. They were built with
+# https://github.com/aspect-build/bazel-lib/blob/main/tools/release/hashes.bzl
+for sha in $(ls artifacts-*/*.sha256); do
+  echo "" >> $sha
+done
+
+mkdir -p ${PREFIX}/tools
+cat >${PREFIX}/tools/integrity.bzl <<EOF
+"Generated during release by release_prep.sh, using integrity.jq"
+
+RELEASED_BINARY_INTEGRITY = $(jq \
+  --from-file .github/workflows/integrity.jq \
+  --slurp \
+  --raw-input artifacts-*/*.sha256 \
+)
+EOF
+
+# Append that generated file back into the archive
+tar --file $ARCHIVE_TMP --append ${PREFIX}/tools/integrity.bzl
+
+# END patch up the archive
+############
+
+gzip < $ARCHIVE_TMP > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
