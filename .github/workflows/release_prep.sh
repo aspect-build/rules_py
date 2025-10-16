a68165112358aac2759148a343b173ad483f1da7
Alex Eagle <alex@aspect.dev>
2025-10-16 10:30:24 -0700

    feat: publish stardocs on releases (#672)
    Co-authored-by: Reid D. McKenzie <arrdem@aspect.build>

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -40,6 +40,14 @@ tar --file $ARCHIVE_TMP --append ${PREFIX}/tools/integrity.bzl
 gzip < $ARCHIVE_TMP > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
+# Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
+# Note, we use xargs here because the repo is on Bazel 7.4 which doesn't have the --output_file flag to bazel query
+docs="$(mktemp -d)"
+bazel --output_base="$docs" query --output=label 'kind("starlark_doc_extract rule", //py/...)' | xargs bazel --output_base="$docs" build
+tar --create --auto-compress \
+    --directory "$(bazel --output_base="$docs" info bazel-bin)" \
+    --file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" .
+
 cat << EOF
 ## Using [Bzlmod] with Bazel 6:
 
