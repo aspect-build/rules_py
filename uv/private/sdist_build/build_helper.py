90e4748c012d88aa89c3849b4416f330b749be15
Reid D McKenzie <me@arrdem.com>
2025-11-06 09:23:12 -0700

    Cleaning up

--- uv/private/sdist_build/build_helper.py
+++ uv/private/sdist_build/build_helper.py
@@ -3,8 +3,7 @@
 from argparse import ArgumentParser
 import shutil
 import sys
-from tempfile import mkdtemp
-from os import chdir, getenv, listdir, path, execv, stat, mkdir
+from os import getenv, listdir, path
 from subprocess import call
 
 # Under Bazel, the source dir of a sdist to build is immutable. `build` and
@@ -23,16 +22,12 @@ PARSER.add_argument("outdir")
 opts, args = PARSER.parse_known_args()
 
 t = getenv("TMPDIR")  # Provided by Bazel
-print("Using tempdir", t, file=sys.stderr)
 
 # Dirty awful way to prevent permissions from being replicated
 shutil.copystat = lambda x, y, **k: None
 shutil.copytree(opts.srcdir, t, dirs_exist_ok=True)
 
 outdir = path.abspath(opts.outdir)
-
-print(listdir(t), file=sys.stderr)
-print(stat(t), file=sys.stderr)
 
 call([
     sys.executable,
