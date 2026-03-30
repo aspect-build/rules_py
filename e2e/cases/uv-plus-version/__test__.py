f49e3cabdff836d009ff28edb19469d423f12947
Adam Cozzette <adamc@aspect.build>
2026-03-30 13:36:23 -0700

    Use jaxlib instead of cowsay for test
    This allows us to make the test a bit more realistic. Now the package version
    truly has a plus in it, as opposed to with cowsay, where we had to artifically
    introduce the plus sign in the uv.lock file.

--- e2e/cases/uv-plus-version/__test__.py
+++ e2e/cases/uv-plus-version/__test__.py
@@ -1,6 +1,8 @@
-import cowsay
+from importlib.metadata import version
 
-# Verify the package can be used - regression test for versions containing '+'
-# Previously, a version like "6.1+local" would produce an invalid Bazel repo
-# name "whl_install__...__6_1+local" because '+' is not allowed in repo names.
-cowsay.cow("hello from +version regression test")
+# Verify the package is installed in the venv - regression test for versions containing '+'
+# Previously, a version like "0.4.25+cuda11.cudnn86" would produce an invalid Bazel repo
+# name because '+' is not allowed in repo names.
+jaxlib_version = version("jaxlib")
+assert jaxlib_version == "0.4.25+cuda11.cudnn86", "version is " + jaxlib_version
+print("jaxlib (version 0.4.25+cuda11.cudnn86) found - regression test passed!")
