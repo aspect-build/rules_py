7ea63be2b181b73a7e20f3404814464d333461af
Jason Bedard <jason+github@jbedard.ca>
2026-09-04 18:04:32 -0700

    fixup tests

--- uv/private/constraints/abi/tests/abi3_test.py
+++ uv/private/constraints/abi/tests/abi3_test.py
@@ -1,10 +1,12 @@
 """Ordinary stable-ABI wheels must not match free-threaded interpreters."""
 
+import importlib.util
 import os
 import sysconfig
 
 freethreaded = bool(sysconfig.get_config_var("Py_GIL_DISABLED"))
 assert freethreaded == (os.environ["EXPECT_ABI3T"] == "1"), freethreaded
 
-for tag in ["ABI3", "ABI3T"]:
-    assert os.environ[tag] == os.environ["EXPECT_" + tag], f"{tag}={os.environ[tag]}"
+for tag in ["abi3", "abi3t"]:
+    matched = importlib.util.find_spec(tag) is not None
+    assert matched == (os.environ["EXPECT_" + tag.upper()] == "1"), f"{tag} matched={matched}"
